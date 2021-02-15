f88304d062c412d5438629aa67d052c44336a3bf
Sourabh choudhary <sourabhskc30@gmail.com>
2021-02-15 21:30:23 +0530

    Before selecting the winner part done

--- script.js
+++ script.js
@@ -26,24 +26,35 @@ window.onload = () =>{ // once window loaded
 
 let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon 
 let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
+let playerSign = "X"; //suppose player will X
 
-// user click function
+// user click function 
 function clickedBox(element){ 
     //console.log(element);
     if(players.classList.contains("player")){ //if players element has contains .player
         element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
         players.classList.add("active");
+        // if player select O then we'll change the playerSign value to O
+        playerSign = "O";
+        element.setAttribute("id", playerSign);      
     }else{
         element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element
         players.classList.add("active");
+        playerSign = "X";
+        element.setAttribute("id", playerSign);
     }
     element.style.pointerEvents = "none"; //once user select any box then that box can't be selected again 
-    
-    bot(); //calling bot function 
+    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed(); //generating random time delay so bot will delay randomly to select box
+  //  console.log(randomDelayTime);
+    setTimeout(()=>{
+        bot(); //calling bot function 
+    }, randomDelayTime); //passing random delay time 
 }
 
 // bot click function
 function bot(){
+    // first change the playerSign...so if user has X value in id then bot will have O
+  //  playerSign = "O";
     let array = []; // creating empty array...we'll store unselected box index in this array
     for(let i = 0; i < allBox.length; i++){
         if(allBox[i].childElementCount == 0){ //if span has no any child element 
@@ -52,15 +63,21 @@ function bot(){
         }
     }
     let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
-    console.log("randomBox " + randomBox);
+  //  console.log("randomBox " + randomBox);
     if(array.length > 0){
         if(players.classList.contains("player")){ //if players element has contains .player
             allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot clicked element
-            players.classList.add("active");
+            players.classList.remove("active");
+            // if user is O then the box id value will be X
+            playerSign = "X";
+            allBox[randomBox].setAttribute("id", playerSign);
         }else{
             allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot clicked element
-            players.classList.add("active");
+            players.classList.remove("active");
+            playerSign = "O";
+            allBox[randomBox].setAttribute("id", playerSign);
         }
     }
     //console.log(array);
+    allBox[randomBox].style.pointerEvents = "none"; //once bot select any box then user can't select or click on that box 
 }
