323ff38f2a4e525482727614a5e8c8131bcb2f44
Sourabh choudhary <sourabhskc30@gmail.com>
2021-02-15 09:31:32 +0530

    Almost done

--- script.js
+++ script.js
@@ -20,13 +20,47 @@ window.onload = () =>{ // once window loaded
     selectOBtn.onclick = ()=>{
         selectBox.classList.add("hide"); //hide the select box on playerO button clicked
         playBoard.classList.add("show"); //show the playboard section on playerO button clicked 
-        players.setAttribute("class", "players active");
+        players.setAttribute("class", "players active player"); //adding three class name in player element
     }
 }
 
 let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon 
-let playerOIcon = "fas fa-circle"; //class name of fontawesome circle icon
+let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
 
-function clickedBox(element){
+// user click function
+function clickedBox(element){ 
+    //console.log(element);
+    if(players.classList.contains("player")){ //if players element has contains .player
+        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element
+        players.classList.add("active");
+    }else{
+        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element
+        players.classList.add("active");
+    }
+    element.style.pointerEvents = "none"; //once user select any box then that box can't be selected again 
+    
+    bot(); //calling bot function 
+}
 
+// bot click function
+function bot(){
+    let array = []; // creating empty array...we'll store unselected box index in this array
+    for(let i = 0; i < allBox.length; i++){
+        if(allBox[i].childElementCount == 0){ //if span has no any child element 
+            array.push(i); //inserting unclicked or unselected boxes inside array means that span has no children  
+            //console.log(i + " " + "has no children");
+        }
+    }
+    let randomBox = array[Math.floor(Math.random() * array.length)]; //getting random index from array so bot will select random unselected box
+    console.log("randomBox " + randomBox);
+    if(array.length > 0){
+        if(players.classList.contains("player")){ //if players element has contains .player
+            allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside bot clicked element
+            players.classList.add("active");
+        }else{
+            allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside bot clicked element
+            players.classList.add("active");
+        }
+    }
+    //console.log(array);
 }
